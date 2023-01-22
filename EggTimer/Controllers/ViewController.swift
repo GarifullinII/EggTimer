1a1ca3497954e9ab7b122bd61f30812cd04437c0
GArifullinII <ildaridrisovich@gmail.com>
2023-01-23 02:01:44 +0400

    feat: added groups and functions

--- EggTimer/Controllers/ViewController.swift
+++ EggTimer/Controllers/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let eggLabel: UILabel = {
+        let label = UILabel()
+        label.text = "How do you like your eggs?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        view.backgroundColor = .red
+        
+        view.backgroundColor = #colorLiteral(red: 0.5203130841, green: 0.8027799726, blue: 0.8295106292, alpha: 1)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        
+    }
+}
+
+extension ViewController {
+    
+    private func setConstraints() {
+        
     }
 }
 
